aa1cd7a05ad51b1f5812e2f7a4fe46ce08db1264
Makiesse Morais <makiesse.dev@gmail.com>
2023-08-09 15:47:42 +0100

    feat: shadcn ui init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="flex items-center justify-center">
+      <Card className="w-[400px] h-[700px]">
+        <CardHeader>
+          <CardTitle>Chat AI</CardTitle>
+          <CardDescription>Using Vercel SDK to create a chat bot.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>message</p>
+        </CardContent>
+        <CardFooter>
+          form
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
